aac1b1ef0ee866b2bbe7dbb691d3054d178e1b09
KaleMakinde <makindeadeyemiadekale@gmail.com>
2023-08-21 21:03:13 +0100

    added cart implementation succesfully

--- src/layout/MainContainer.jsx
+++ src/layout/MainContainer.jsx
@@ -12,7 +12,7 @@ const MainContainer = () => {
   const [{ foodItems, cartShow }, dispatch] = useStateValue();
   const [scrollValue, setScrollValue] = useState(0);
 
-  useEffect(() => { }, [scrollValue, cartShow]);
+  useEffect(() => {}, [scrollValue, cartShow]);
 
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
